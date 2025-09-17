62444d885df5e77771dc7058ba599fa4da112ac3
Vaghela Madhuri <madhurivaghela463@gmail.com>
2025-09-17 20:21:13 +0530

    If_condition.java
    Signed-off-by: Vaghela Madhuri  <madhurivaghela463@gmail.com>

---
+++ If_condition.java
@@ -0,0 +1,15 @@
+//package ✓
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		//condition statement
+		Scanner sc = new Scanner(System.in);
+		int age = sc.nextInt();
+
+		if (age > 18) {
+			System.out.println("age is grater");
+		} else {
+			System.out.println("age is lesser");
+		}
+	}
+}
