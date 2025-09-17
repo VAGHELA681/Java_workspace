3caccea29ea0764cc5c8e89b1b8355e745543e35
Vaghela Madhuri <madhurivaghela463@gmail.com>
2025-09-17 15:00:01 +0530

    while.java
    Signed-off-by: Vaghela Madhuri  <madhurivaghela463@gmail.com>

---
+++ while.java
@@ -0,0 +1,18 @@
+//class 
+public class Main {
+	public static void main(String[] args) {
+		//method
+		// will loop do whill loop
+
+		int i = 100; 
+		//while (i < 100) {
+			//System.out.println(i);
+			//i++;
+		//}
+		
+		do {
+		    System.out.println(i);
+		    i = i+1;
+		    } while (i<10);
+	}
+}
