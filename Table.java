88a7d2befa2206da0d34e94aaf67ac5a2edb7205
Vaghela Madhuri <madhurivaghela463@gmail.com>
2025-09-17 15:03:13 +0530

    Table.java
    Signed-off-by: Vaghela Madhuri  <madhurivaghela463@gmail.com>

---
+++ Table.java
@@ -0,0 +1,13 @@
+//package  
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		//table
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		for (int i=0; i<11; i++) {
+		System.out.println(i + " = "+ " "+(num*i));
+		}
+	}
+}
