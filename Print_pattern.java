09a925e87c77abba3936e9c1d049748e06ad3519
Vaghela Madhuri <madhurivaghela463@gmail.com>
2025-09-17 16:56:15 +0530

    Print_pattern.java
    Signed-off-by: Vaghela Madhuri  <madhurivaghela463@gmail.com>

---
+++ Print_pattern.java
@@ -0,0 +1,11 @@
+//class √
+public class Main {
+	public static void main(String[] args) {
+		//print the pattern
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		System.out.println("******");
+	}
+}
