764b162c77039be1fac1520b0fead7b95474ca01
Vaghela Madhuri <madhurivaghela463@gmail.com>
2025-09-17 16:52:00 +0530

    String1.java
    Signed-off-by: Vaghela Madhuri  <madhurivaghela463@gmail.com>

---
+++ String1.java
@@ -0,0 +1,24 @@
+//package √
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		//user input
+		//String name = sc.next();
+		//String name = sc.nextLine();
+		//System.out.print(name);
+		
+		//print
+		//System.out.print("madhuri");
+		
+		//println
+		//System.out.println("hello");
+		//System.out.println("meera");
+		
+		String f_name =  "madhuri" ;
+		String l_name = "vaghela" ;
+		String fullname = f_name + l_name;
+		System.out.print(fullname);
+	}
+}
