486010cdbe8e03d4040b0d7709b5f88aaf7b13b5
Vaghela Madhuri <madhurivaghela463@gmail.com>
2025-09-17 16:41:45 +0530

    String2.java
    Signed-off-by: Vaghela Madhuri  <madhurivaghela463@gmail.com>

---
+++ String2.java
@@ -0,0 +1,16 @@
+//class 
+public class Main {
+	public static void main(String[] args) {
+		String firstName = "Tony";
+
+		String secondName = "Stark";
+
+
+		String fullName = firstName + " " + secondName;
+
+		 System.out.println(fullName.length());
+		 
+		for (int i=0; i <fullName.length(); i++) {
+   	System.out.println(fullName.charAt(i));     	  }
+	}
+}
