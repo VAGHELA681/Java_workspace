ffa208616e7401e15f0511d2c07706a4fa48b1c2
Vaghela Madhuri <madhurivaghela463@gmail.com>
2025-09-17 20:26:16 +0530

    Function2.java
    Signed-off-by: Vaghela Madhuri  <madhurivaghela463@gmail.com>

---
+++ Function2.java
@@ -0,0 +1,38 @@
+//package com.apnacollege; ✓
+import java.util.*;
+
+public class function {
+
+	//A METHOD to calculate sum of 2 numbers - a & b
+
+
+	//public static void sum(int a, int b) {
+	    public static int multiplication(int a, int b) {
+
+		int multiplication= a * b;
+
+
+		System.out.println(multiplication);
+		
+		return 1 ;
+
+
+	}
+
+
+
+	public static void main(String[] args) {
+
+		int a = 10;
+
+		int b = 20;
+
+		multiplication(a, b); // Function Call
+		
+
+
+
+	}
+
+}
+
