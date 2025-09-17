38743af1a010ab2f44e6d639f21407e6fce2b9df
Vaghela Madhuri <madhurivaghela463@gmail.com>
2025-09-17 15:05:55 +0530

    Sum.java
    Signed-off-by: Vaghela Madhuri  <madhurivaghela463@gmail.com>

---
+++ Sum.java
@@ -0,0 +1,13 @@
+//package  
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		//take a variable a&b and print the sum
+		Scanner sc = new Scanner (System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int  sum = a + b ;
+		System.out.print(sum);
+		
+	}
+}
