e6d10d7dd86afe1cad3d1e9cf9c3bb79c9d4767d
Vaghela Madhuri <madhurivaghela463@gmail.com>
2025-09-18 15:40:20 +0530

    2dArray.java
    Signed-off-by: Vaghela Madhuri  <madhurivaghela463@gmail.com>

---
+++ 2dArray.java
@@ -0,0 +1,25 @@
+// package ✓
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int row = sc.nextInt();
+		int col = sc.nextInt() ;
+		int number [][] = new int [row][col];
+		// input
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                number [i][j] = sc.nextInt();
+            }
+        }
+        //output
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                 System.out.print(number[i][j]);
+            }
+        }
+        
+           
+       
+	}
+}
