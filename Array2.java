4a9e1388e9e9962224ad8c3554a07ac83abec2be
Vaghela Madhuri <madhurivaghela463@gmail.com>
2025-09-18 15:47:44 +0530

    Array2.java
    Signed-off-by: Vaghela Madhuri  <madhurivaghela463@gmail.com>

---
+++ Array2.java
@@ -0,0 +1,21 @@
+//package ✓
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		//Scanner sc = new Scanner(System.in);
+		int marks[] = new int [3] ;
+		//int [] marks = new int [3] ;
+		//int [] marks =  {89,90,100};
+		marks[0] = 89;
+		marks[1] = 90;
+		marks[2] = 100;
+
+		//System.out.println(marks[0]);
+		//System.out.println(marks[1]);
+		//System.out.println(marks[2]);
+
+		for (int i = 0; i <= 2; i++) {
+			System.out.println(marks[i]);
+		}
+	}
+}
