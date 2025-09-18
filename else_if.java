1079aea9045a3fc9a90fe448ff3cd0f1cbcba282
Vaghela Madhuri <madhurivaghela463@gmail.com>
2025-09-18 15:38:28 +0530

    else_if.java
    Signed-off-by: Vaghela Madhuri  <madhurivaghela463@gmail.com>

---
+++ else_if.java
@@ -0,0 +1,19 @@
+//package ✓
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		//else if condition
+		Scanner sc = new Scanner(System.in);
+		int button = sc.nextInt();
+
+		if (button == 1) {
+			System.out.print("good morning");
+		} else if (button == 2) {
+			System.out.print("good afternoon");
+		} else if (button == 3) {
+			System.out.print("good evening");
+		} else {
+			System.out.print("nothing");
+	    }
+	}
+}
