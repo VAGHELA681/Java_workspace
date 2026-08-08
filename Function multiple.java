3ce77e1665aef7177ee2d873c331181eee558da8
Vaghela Madhuri <madhurivaghela463@gmail.com>
2026-08-08 21:07:22 +0530

    Function multiple.java

---
+++ Function multiple.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter b: ");
+        int b = sc.nextInt();
+
+        int result = multiply(a, b);
+
+        System.out.println("Multiplication = " + result);
+    }
+}
