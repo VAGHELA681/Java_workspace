91396565f1a841adfc8801751c9ce2e56950f328
Vaghela Madhuri <madhurivaghela463@gmail.com>
2026-08-08 14:20:00 +0530

    If_condition.java
    An if condition in java is used to check a condition if the condition true the code inside the if block is expected 
    
    if (condition) {
        // code
    }

---
+++ If_condition.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 29;
+		
+		if (a>b) {
+		    System.out.println("a is greater");
+		} else {
+		    System.out.println("b is greater");
+	    }
+	}
+}
+
+
