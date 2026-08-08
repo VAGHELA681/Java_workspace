ec4c31821bdf4d4dbcb755a3df8c9bc616e429dc
Vaghela Madhuri <madhurivaghela463@gmail.com>
2026-08-08 16:33:36 +0530

    Array.java
    An array is a collection of multiple values of the same data type stored in a single variable.
    
    Definition:
    An array is a fixed-size data structure used to store multiple elements of the same type.

---
+++ Array.java
@@ -0,0 +1,28 @@
+public class Main {
+	public static void main(String[] args) {
+		//int[] marks = new int[3];
+		//int [] marks = {85 , 90 , 95};
+
+		//marks[0] = 85;
+		//marks[1] = 90;
+		//marks[2] = 95;
+
+		//System.out.println(marks[0]); // 85
+		//System.out.println(marks[1]); // 90
+		//System.out.println(marks[2]); // 95
+
+		//for (int i = 0; i < 3; i++) {
+		//System.out.println(marks[i]); // 95
+		//}
+
+		String[] subject = new String[3];
+
+		subject[0] = "java";
+		subject[1] = "python";
+		subject[2] = "c++";
+
+		System.out.println(subject[0]);
+		System.out.println(subject[1]);
+		System.out.println(subject[2]);
+	}
+}
