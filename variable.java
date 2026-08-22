5fd5439005e22edbf24cbd37b48a818ae63f785a
Vaghela Madhuri <madhurivaghela463@gmail.com>
2026-08-22 15:39:47 +0530

    variable.java
    Name: Amit
    Age: 25
    Salary: 30000.5

---
+++ variable.java
@@ -0,0 +1,12 @@
+class Employee {
+    public static void main(String[] args) {
+
+        String name = "Amit";
+        int age = 25;
+        double salary = 30000.50;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+    }
+}
