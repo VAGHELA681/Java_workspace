76b950130c94c7fac0a3874c5b77113c27bc7b97
Vaghela Madhuri <madhurivaghela463@gmail.com>
2026-08-22 16:03:03 +0530

    if condition.java

---
+++ if condition.java
@@ -0,0 +1,12 @@
+class Student {
+    public static void main(String[] args) {
+
+        int marks = 35;
+
+        if (marks >= 40) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
