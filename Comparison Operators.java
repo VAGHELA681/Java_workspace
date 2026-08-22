07b6409ebe26ef97a5981924a9eb5df6c7e48eb2
Vaghela Madhuri <madhurivaghela463@gmail.com>
2026-08-22 15:53:33 +0530

    Comparison Operators.java
    false
    true
    false
    true

---
+++ Comparison Operators.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+
+		System.out.println(a > b);   // false
+		System.out.println(a < b);   // true
+		System.out.println(a == b);  // false
+		System.out.println(a != b);  // true
+	}
+}
