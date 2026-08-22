328db306640796d71076e1cfcebb1d6941284edf
Vaghela Madhuri <madhurivaghela463@gmail.com>
2026-08-22 16:01:21 +0530

    If_else_condition.java
    You can vote
    
    age = 20
    
    age >= 18
    20 >= 18
    true

---
+++ If_else_condition.java
@@ -0,0 +1,12 @@
+class Demo {
+    public static void main(String[] args) {
+
+        int age = 20;
+
+        if (age >= 18) {
+            System.out.println("You can vote");
+        } else {
+            System.out.println("You cannot vote");
+        }
+    }
+}
