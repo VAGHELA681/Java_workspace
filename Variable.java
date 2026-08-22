8d77c36fae4cee26a92ce8ad9a282c3408751d8c
Vaghela Madhuri <madhurivaghela463@gmail.com>
2026-08-22 15:38:37 +0530

    Variable.java
    A variable is a named memory location used to store data.
    
    int → data type
    age → variable name
    20 → value
    
    A variable in Java is a named storage location that holds a value of a specific data type.

---
+++ Variable.java
@@ -0,0 +1,10 @@
+class Demo {
+    public static void main(String[] args) {
+
+        int age = 20;
+        String name = "Rahul";
+
+        System.out.println(age);
+        System.out.println(name);
+    }
+}
