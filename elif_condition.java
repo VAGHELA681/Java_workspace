3b1220bd236714ece806539d653089b13ab6f949
Vaghela Madhuri <madhurivaghela463@gmail.com>
2026-08-22 16:06:36 +0530

    elif_condition.java
    if-else is a conditional statement used to execute different blocks of code based on whether a condition is true or false.

---
+++ elif_condition.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) {
+		int marks = 75;
+
+		if (marks >= 90) {
+			System.out.println("A+");
+		} else if (marks >= 60) {
+			System.out.println("A");
+		} else if (marks >= 40) {
+			System.out.println("B");
+		} else {
+			System.out.println("Fail");
+		}
+	}
+}
