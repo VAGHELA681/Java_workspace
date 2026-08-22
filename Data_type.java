48db5f689bbd4152478167184305f90e87e4520a
Vaghela Madhuri <madhurivaghela463@gmail.com>
2026-08-22 15:42:34 +0530

    Data_type.java
    A data type tells Java what type of value a variable can store.
    
    Data Types
    ├── Primitive
    └── Non-Primitive
    
    25
    30000.5
    A
    true

---
+++ Data_type.java
@@ -0,0 +1,14 @@
+class Demo {
+    public static void main(String[] args) {
+
+        int age = 25;
+        double salary = 30000.50;
+        char grade = 'A';
+        boolean passed = true;
+
+        System.out.println(age);
+        System.out.println(salary);
+        System.out.println(grade);
+        System.out.println(passed);
+    }
+}
